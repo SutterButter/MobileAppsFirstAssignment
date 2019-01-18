8f5910948d92c546d33e92b0d25961a60b5d3df6
SutterButter <aeontruths@gmail.com>
2019-01-18 12:27:46 -0500

    Initial Commit

---
+++ MobileAppsFirstAssignment/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MobileAppsFirstAssignment
+//
+//  Created by Noah Sutter on 1/18/19.
+//  Copyright © 2019 Noah Sutter. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

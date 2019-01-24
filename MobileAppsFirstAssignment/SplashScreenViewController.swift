c9c844fd086f61e4a8e98fe709d9e127b060c4da
SutterButter <aeontruths@gmail.com>
2019-01-24 14:36:23 -0500

    Added splash screen and login screen (login not functional)

---
+++ MobileAppsFirstAssignment/SplashScreenViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SplashScreenViewController.swift
+//  MobileAppsFirstAssignment
+//
+//  Created by Noah Sutter on 1/23/19.
+//  Copyright © 2019 Noah Sutter. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        performSegue(withIdentifier: "AuthNeeded", sender: self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MobileAppsFirstAssignment' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MobileAppsFirstAssignment
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+
+  target 'MobileAppsFirstAssignmentTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MobileAppsFirstAssignmentUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end

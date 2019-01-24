c553ef00d4f811d6c41f2d6ed324f3deb68dfcb2
SutterButter <aeontruths@gmail.com>
2019-01-24 15:39:36 -0500

    Added basic ability to login through firebase with email/password

--- MobileAppsFirstAssignment/LoginViewController.swift
+++ MobileAppsFirstAssignment/LoginViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +23,30 @@ class LoginViewController: UIViewController {
     }
     
 
+    @IBAction func loginPressed(_ sender: Any) {
+        // check email not empty
+        if let email = self.emailField.text, email != "", let password = self.passwordField.text, password != "" {
+            // Start loading the user
+            loadingIndicator.startAnimating()
+            Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                // Done loading user
+                self.loadingIndicator.stopAnimating()
+                
+                // If error show error
+                if let error = error {
+                    let alert = UIAlertController(title: "Problems Logging In", message: error.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    // do nothing
+                }
+            }
+        } else { // if email or password is emtpy display message
+            let alert = UIAlertController(title: "Email/Password Empty", message: "email/password can't be empty", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     /*
     // MARK: - Navigation
 

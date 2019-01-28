7df2ca74f8122c7ca3b289d57f4e28271f7e18b8
SutterButter <aeontruths@gmail.com>
2019-01-28 04:18:14 -0500

    Added ability to take pictures load primary colors of image

--- MobileAppsFirstAssignment/ImageDetectViewController.swift
+++ MobileAppsFirstAssignment/ImageDetectViewController.swift
@@ -9,28 +9,40 @@
 import UIKit
 import Alamofire
 
-class ImageDetectViewController: UIViewController {
+class ImageDetectViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet weak var selectedImage: UIImageView!
     
+    @IBOutlet weak var primaryColor: UIImageView!
+    @IBOutlet weak var secondaryColor: UIImageView!
+    @IBOutlet weak var tirtiaryColor: UIImageView!
+    
+    var passedImage: UIImage!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        makeGetCall(imageURL: "https://sightengine.com/assets/img/examples/example2.jpg")
+        self.selectedImage.image = passedImage
+        makeGetCall()
     }
+   
     
-    
-    func makeGetCall(imageURL: String) {
-        
+    func makeGetCall() {
         
         // Set up the URL request
         //let endpoint = "https://api.sightengine.com/1.0/properties.json"
         let endpoint: String = "https://api.sightengine.com/1.0/properties.json?api_user=***REMOVED***&api_secret=***REMOVED***"//&url=\//(imageURL)"
+
+        var quality = 0.75
+        var imageData = selectedImage.image?.jpegData(compressionQuality: CGFloat(quality))
+        while ((imageData?.count)! / 1048576 > 10) {
+            quality = quality - 0.05
+            print(quality)
+            imageData = selectedImage.image?.jpegData(compressionQuality: CGFloat(quality))
+        }
         
         
-        let imageData = selectedImage.image?.jpegData(compressionQuality: 1)
         
         Alamofire.upload(
             multipartFormData: { multipartFormData in
@@ -42,13 +54,77 @@ class ImageDetectViewController: UIViewController {
                 case .success(let upload, _, _):
                     upload.responseJSON { response in
                         debugPrint(response)
+                        guard response.result.isSuccess else {
+                            print("Error while fetching colors")
+                            return
+                        }
+                        
+                        guard let value = response.result.value as? [String: Any],
+                            let colors = value["colors"] as? [String: Any?],
+                            let dominant = colors["dominant"] as? [String: Any],
+                            let dominantHex = dominant["hex"] as? String,
+                            let others = colors["other"] as? [[String: Any]]
+                        else {
+                                print("Missing data")
+                                return
+                        }
+                        
+                        var secondaryHex = "FFFFFF"
+                        var tirtiaryHex = "FFFFFF"
+                        if others.count >= 2 {
+                            secondaryHex = others[0]["hex"] as! String
+                            tirtiaryHex = others[1]["hex"] as! String
+                        } else if others.count >= 1 {
+                            secondaryHex = others[0]["hex"] as! String
+                        }
+                        
+                        self.primaryColor.image = UIImage.imageWithColor(tintColor: UIColor(hexString: dominantHex)!)
+                        self.secondaryColor.image = UIImage.imageWithColor(tintColor: UIColor(hexString: secondaryHex)!)
+                        self.tirtiaryColor.image = UIImage.imageWithColor(tintColor: UIColor(hexString: tirtiaryHex)!)
+                        
+                        print(colors)
+                        print(dominant)
+                        print(dominantHex)
+                        print(others)
+                        
                     }
                 case .failure(let encodingError):
                     print(encodingError)
                 }
         })
-        
-        
-        
+    }
+    
+}
+extension UIImage {
+    static func imageWithColor(tintColor: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+        tintColor.setFill()
+        UIRectFill(rect)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
+extension UIColor {
+    convenience init?(hexString: String) {
+        var chars = Array(hexString.hasPrefix("#") ? hexString.dropFirst() : hexString[...])
+        let red, green, blue, alpha: CGFloat
+        switch chars.count {
+        case 3:
+            chars = chars.flatMap { [$0, $0] }
+            fallthrough
+        case 6:
+            chars = ["F","F"] + chars
+            fallthrough
+        case 8:
+            alpha = CGFloat(strtoul(String(chars[0...1]), nil, 16)) / 255
+            red   = CGFloat(strtoul(String(chars[2...3]), nil, 16)) / 255
+            green = CGFloat(strtoul(String(chars[4...5]), nil, 16)) / 255
+            blue  = CGFloat(strtoul(String(chars[6...7]), nil, 16)) / 255
+        default:
+            return nil
+        }
+        self.init(red: red, green: green, blue:  blue, alpha: alpha)
     }
 }
--- MobileAppsFirstAssignment/LandingPageViewController.swift
+++ MobileAppsFirstAssignment/LandingPageViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LandingPageViewController: UIViewController {
+class LandingPageViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +16,60 @@ class LandingPageViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
+    var selectedImage: UIImage!
+    
+    @IBAction func takePhotoPressed(_ sender: Any) {
+        let vc = UIImagePickerController()
+        if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            let alertController = UIAlertController.init(title: nil, message: "Device has no camera.", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction.init(title: "OK", style: .default, handler: {(alert: UIAlertAction!) in
+            })
+            
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+            
+        }
+        else{
+            vc.sourceType = .camera
+        }
+        
+        vc.allowsEditing = false
+        vc.delegate = self
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.originalImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        
+        
+        
+        self.selectedImage = image
+        
+        // print out the image size as a test
+        print(image.size)
+        
+        picker.dismiss(animated: true)
+        
+        DispatchQueue.main.async(){
+            self.performSegue(withIdentifier: "photoChosenSegue", sender: self)
+        }
+        return
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("Preparing")
+        if segue.identifier == "photoChosenSegue" {
+            let dvc = segue.destination as! ImageDetectViewController
+            dvc.passedImage = selectedImage
+        }
+    }
     /*
     // MARK: - Navigation
 
